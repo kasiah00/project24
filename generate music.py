1a06e64c4f88d74a8ee93a16a50e79a0669cfe81
kasiah00 <saehyun3818@gmail.com>
2024-12-20 17:07:16 +0900

    Update generate music.py

--- generate music.py
+++ generate music.py
@@ -1,9 +1,20 @@
+import numpy as np
+import tensorflow as tf
+from music21 import converter, instrument, note, chord, stream
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Activation
+from tensorflow.keras.utils import to_categorical
+
 def generate_music(model, seed_sequence, note_to_int, int_to_note, length):
+    """Generates music using the trained model and a seed sequence."""
     output = []
+    sequence = seed_sequence
     for _ in range(length):
-        prediction = model.predict(seed_sequence)
+        prediction = model.predict(sequence, verbose=0)
         index = np.argmax(prediction)
-        output.append(int_to_note[index])
-        seed_sequence = np.roll(seed_sequence, -1, axis=1)
-        seed_sequence[0, -1] = index
+        result = int_to_note[index]
+        output.append(result)
+
+        sequence = np.roll(sequence, -1, axis=1)
+        sequence[0, -1, 0] = index / float(len(note_to_int))
     return output
